8f6a02cb6f05ed64be654d701628aac8a9dbd475
Jagruti Patil <103519235+jagrutipatilp@users.noreply.github.com>
2024-11-06 15:42:32 +0530

    Update 0-1_knapsack.cpp

--- 0-1_knapsack.cpp
+++ 0-1_knapsack.cpp
@@ -1,42 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int n=0;
-    cout<<"Enter the number of items"<<endl;
-    cin>>n;
+int main() {
+    int n = 0;
+    cout << "Enter the number of items: ";
+    cin >> n;
 
-    int capacity=0;
-    cout<<"Enter the capacity of knapsack"<<endl;
-    cin>>capacity;
+    int capacity = 0;
+    cout << "Enter the capacity of knapsack: ";
+    cin >> capacity;
 
-    int price[n+1]={0};
-    int weight[capacity+1]={0};
-    int dp[n+1][capacity+1];
+    int price[n + 1] = {0};
+    int weight[n + 1] = {0};  // Corrected: array size should be n+1, not capacity
+    int dp[n + 1][capacity + 1];
 
-    for(int i=1;i<n+1;i++){
-        cout<<"Enter the profit and weight of item "<<i<<endl;
-        cin>>price[i]>>weight[i];
+    // Input profits and weights
+    for (int i = 1; i <= n; i++) {
+        cout << "Enter the profit and weight of item " << i << ": ";
+        cin >> price[i] >> weight[i];
     }
 
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=capacity;j++){
-            if(i==0 || j==0){
-                dp[i][j]=0;
-            }else if(weight[i]<=j){
-                dp[i][j] = max(dp[i-1][j], price[i]+dp[i-1][j-weight[i]]);
-            }else{
-                dp[i][j] = dp[i-1][j];
+    // Initialize DP table
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= capacity; j++) {
+            if (i == 0 || j == 0) {
+                dp[i][j] = 0;
+            } else if (weight[i] <= j) {
+                dp[i][j] = max(dp[i - 1][j], price[i] + dp[i - 1][j - weight[i]]);
+            } else {
+                dp[i][j] = dp[i - 1][j];
             }
         }
     }
 
-    // for(int i=0;i<=n;i++){
-    //     for(int j=0;j<=capacity;j++){
-    //         cout<<dp[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-
-    cout<<"The maximum profit is: "<<dp[n][capacity];
+    cout << "The maximum profit is: " << dp[n][capacity] << endl;
+    return 0;
 }
